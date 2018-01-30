56c063b8a2b566c870de8b96047b1e30b03da0a7
Kala30 <caleb.yun@gmail.com>
2018-01-30 09:26:06 -0800

    Unit 6 Day 1

--- Yun Caleb Unit 5-5 Recursion/Exercises.java
+++ Yun Caleb Unit 5-5 Recursion/Exercises.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Exercises here.
+ * Recursion Coding Assignment
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Iain Ro, Caleb Yun 
+ * @version 1/30/2018
  */
 public class Exercises
 {
@@ -12,6 +12,21 @@ public class Exercises
         for(int i = 0; i < n; i++)
         {
             System.out.print("*");
+        }
+    }
+
+    private static int orig = 1;
+
+    public static void printTriangle(int num)
+    {
+        if(num == 1)
+            printStars(orig);
+        else
+        {
+            printStars(orig);
+            orig++;
+            System.out.println();
+            printTriangle(num - 1);
         }
     }
 
@@ -25,16 +40,62 @@ public class Exercises
             reverseTriangle(num - 1);
         }
     }
-    
-    public static void printTriangle(int num)
+
+    public static int iterMultiplication(int a, int b)
     {
-        if(num == 1)
-            printStars(num);
+        int sum = 0;
+        for (int i = b; i > 0; i--)
+        {
+            sum += a;
+        }
+        return sum;
+    }
+
+    public static int recursiveMultiplication(int a, int b)
+    {
+
+        if(b == 1)return a;
         else
         {
-            printStars(num - (num-1));
-            System.out.println();
-            printTriangle(num - 1);
+            return a + recursiveMultiplication(a, b-1);
         }
     }
+
+    public static int power(int x, int y)
+    {
+        if (y == 0)
+            return 1;
+        else {
+            return x * power(x, y - 1);
+        }
+    }
+
+    public static int numberOfDigits(int num)
+    {
+        if(num<10)return 1;
+        else
+        {
+            return 1 + numberOfDigits(num / 10);
+        }
+    }
+
+    public static int simplifyAngle(int angle)
+    {
+        if (angle < 360)
+            return angle;
+        else {
+            return simplifyAngle(angle-360);
+        }
+    }
+    
+    public static int fibonacci(int n)
+    {
+        if (n == 1 || n == 2)
+            return 1;
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    
+    
 }
