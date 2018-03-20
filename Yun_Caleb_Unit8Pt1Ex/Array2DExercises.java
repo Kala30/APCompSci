eaa76646401761371229b8394aa6cf698d8216d5
Kala30 <caleb.yun@gmail.com>
2018-03-20 16:59:18 -0700

    U8 A3

--- Yun_Caleb_Unit8Pt1Ex/Array2DExercises.java
+++ Yun_Caleb_Unit8Pt1Ex/Array2DExercises.java
@@ -44,7 +44,7 @@ public class Array2DExercises
             System.out.print("\n");
         }
     }
-    
+
     /**
      * Returns the sum of a specified row in arr.
      */
@@ -56,23 +56,56 @@ public class Array2DExercises
         }
         return sum;
     }
-    
+
     /**
      * Returns the sum of the elements in column col of arr.
      */
     public static int columnSum(int[][] arr, int col)
     {
         int sum = 0;
-        for (int row : arr) {
+        for (int[] row : arr) {
             if (col < row.length)
                 sum += row[col];
         }
         return sum;
     }
-    
+
     /**
      * Returns the value of the largest positive element in
      * the 2D array m, or 0, if all its elements are negative.
      */
     private static double positiveMax(double[][] m)
+    {
+        double largest = 0;
+        for (double[] row : m)
+        {
+            for (double num : row)
+            {
+                if (num > 0 && num > largest)
+                    largest = num;
+            }
+        }
+
+        return largest;
+    }
+
+    /**
+     * Returns true if m1 "covers" m2, false otherwise.
+     * Precondition: m1 and m2 have the same dimensions.
+     */
+    private static boolean covers(double[][] m1, double[][] m2)
+    {
+        int count = 0;
+        for (int i = 0; i < m1.length; i++)
+        {
+            for (int j = 0; j < m1[i].length; j++)
+            {
+                if (m1[i][j] != m2[i][j])
+                    count++;
+                if (count > (m1.length * m1[i].length)/2)
+                    return false;
+            }
+        }
+        return true;
+    }
 }
