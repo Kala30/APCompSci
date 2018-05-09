08d25a57fd64e4fb77c22264b599a25816f778ae
Kala30 <caleb.yun@gmail.com>
2018-05-08 22:56:04 -0700

    U10 A2

--- Yun Caleb Unit10Ex/Unit10.java
+++ Yun Caleb Unit10Ex/Unit10.java
@@ -1,6 +1,6 @@
 import java.util.Collections;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Unit 10 Exercises
@@ -16,6 +16,12 @@ public class Unit10
         System.out.println("Searching for 5 in 0-9: " + search(arr, 0, 9, 1));
         System.out.println("Searching for 5 in 0-4: " + search(arr, 0, 5, 1));
         System.out.println("Searching for 5 in 5-9: " + search(arr, 5, 9, 1));
+        
+        System.out.println("\nShuffle");
+        String[] arr2 = {"0","1","2","3","4","5","6","7","8","9"};
+        //Integer[] arr2 = {0,1,2,3,4,5,6,7,8,9};
+        System.out.println("{0,1,2,3,4,5,6,7,8,9}");
+        shuffle(arr2);
     }
     
     public static int search(int[] a, int m, int n, int target)
@@ -37,5 +43,16 @@ public class Unit10
             else
                 return -1;
         }
+    }
+    
+    public static void shuffle(Object[] arr) {
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            int randInt = rand.nextInt(arr.length - i) + i;
+            Object temp = arr[randInt];
+            arr[randInt] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
